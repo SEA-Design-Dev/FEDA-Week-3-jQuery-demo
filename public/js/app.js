d3c811922264105f9b521357372eaf65a293aff2
Jed Foster <jed@jedfoster.com>
2015-09-17 11:49:09 -0700

    hide/show tagged content

--- public/js/app.js
+++ public/js/app.js
@@ -5,5 +5,16 @@ $('[data-show-tag]').click(function(event) {
   var tag = $(event.currentTarget).data('show-tag');
 
   console.log(tag);
+
+  if(tag == 'all') {
+    $('[data-tags]').show();
+    return;
+  }
+
+  // Hide ALL the tagged content, we'll un-hide next
+  $('[data-tags]').hide();
+
+  // Show all tagged content that CONTAINS tag
+  $('[data-tags*="' + tag + '"]').show();
 });
 
